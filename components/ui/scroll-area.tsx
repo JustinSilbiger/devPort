8d81dae974f319339a6a1785768a520d79c827ae
Justin Silbiger <justin@example.com>
2026-06-10 18:32:51 -0400

    feat: improve UI component exports for better tree-shaking

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -1,23 +1,53 @@
-"use client"
+ "use client"
 
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
-
+import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from "@/lib/utils"
+
+const scrollAreaVariants = cva(
+  "relative overflow-hidden",
+  {
+    variants: {
+      orientation: {
+        vertical: "",
+        horizontal: "",
+      },
+    },
+    defaultVariants: {
+      orientation: "vertical",
+    },
+  }
+)
+
+const scrollBarVariants = cva(
+  "flex touch-none select-none transition-colors",
+  {
+    variants: {
+      orientation: {
+        vertical: "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        horizontal: "h-2.5 border-t border-t-transparent p-[1px]",
+      },
+    },
+    defaultVariants: {
+      orientation: "vertical",
+    },
+  }
+)
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & VariantProps<typeof scrollAreaVariants>
+>(({ className, orientation, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
-    className={cn("relative overflow-hidden", className)}
+    className={cn(scrollAreaVariants({ orientation }), className)}
     {...props}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    <ScrollBar orientation={orientation} />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
@@ -25,17 +55,17 @@ ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar> & VariantProps<typeof scrollBarVariants>
+>({
+  className,
+  orientation,
+  ...props
+}, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
-      orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
-      orientation === "horizontal" &&
-        "h-2.5 border-t border-t-transparent p-[1px]",
+      scrollBarVariants({ orientation }),
       className
     )}
     {...props}
